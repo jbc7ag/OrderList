dbd0f8c19a27301d4e4eb34406d029d89a8d192c
Daniel Briones <Daniel Briones@compac.com.mx>
2017-09-22 20:30:31 -0500

    Carga inicial del proyecto

---
+++ ConsoleApplication1/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Producto
+    {
+        public string Nombre{ get; set; }
+        public double Precio { get; set; }
+        public string Descripcion { get; set; }
+
+        public Producto(string aNombre, double aPrecio, string aDescripcio)
+        {
+            Nombre = aNombre;
+            Precio = aPrecio;
+            Descripcion = aDescripcio;
+        }
+    }
+}
